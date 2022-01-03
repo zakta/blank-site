a6959aea89e87e9e5edce847d61f57e0d07839a7
Alexandra <bguevaraale@gmail.com>
2022-01-03 13:08:27 -0300

    ci: add editor config & close #1

---
+++ components/GlobalStyle/styles.js
@@ -0,0 +1,27 @@
+// 3rd parties
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    background: white;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    min-width: 320px;
+
+    h2 {
+      font-size: 40px;
+      color: #1a3968;
+      margin: 0;
+      text-transform: uppercase;
+      margin: 0;
+      line-height: 50px;
+    }
+  }
+`;
+
+export default GlobalStyle;
