91d2fe8e08649d631e3f946fb09d7174ad6beae6
Alexandra <bguevaraale@gmail.com>
2022-03-02 14:41:21 -0300

    feat: add component for responsive modal

--- pages/modal.js
+++ pages/modal.js
@@ -1,45 +1,16 @@
 // 3rd parties
-import Modal from '@/components/Modal';
-import { Btn } from '@/components/Modal/styles';
-import ModalMobile from '@/components/ModalMobile';
 import Head from 'next/head';
-import { useState } from 'react';
-import Media from 'react-media';
+// components
+import ModalResponsive from '@/components/modalResponsive';
 
 export default function Home() {
-  const [activeModal, setActiveModal] = useState(false);
-  const [open, setOpen] = useState(false);
-
   return (
     <div>
       <Head>
         <title>Modal</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Media queries={{ small: '( max-width: 599px)', medium: '(min-width: 600px)' }}>
-        { (matches) => (
-          <>
-            {matches.small
-            && (
-            <>
-              <Btn type="button" onClick={() => setOpen(true)}>Abrir Mobile</Btn>
-              <ModalMobile activeModal={open} setActiveModal={setOpen} />
-            </>
-            )}
-            {matches.medium
-            && (
-            <>
-              <Btn type="button" onClick={() => setActiveModal(true)}>Abrir Desktop</Btn>
-              <Modal
-                activeModal={activeModal}
-                setActiveModal={setActiveModal}
-                ariaHideApp={false}
-              />
-            </>
-            )}
-          </>
-        )}
-      </Media>
+      <ModalResponsive />
     </div>
   );
 }
